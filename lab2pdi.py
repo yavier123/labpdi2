2044b53c58b9d2509d7a3df339a2bd55c583f0bd
yavier123 <yavier123>
2016-04-22 04:15:52 +0000

    primera subida del projecto

---
+++ lab2pdi.py
@@ -0,0 +1,30 @@
+#! /usr/bin/python
+
+from SimpleCV  import Image, Camera, Display, time
+
+import matplotlib.pyplot as plt
+
+cam = Camera()
+img = cam.getImage()
+
+img.save("foto1.jpg")
+
+time.sleep(2)
+
+img2=cam.getImage()
+
+img2.save("foto2.jpg")
+
+time.sleep(2)
+
+img3=cam.getImage()
+  
+escaladegris=img2.grayscale()
+escaladegris.save("fotogray.jpg")
+
+histograma=escaladegris.histogram().show()
+
+histo= plt.plot(histograma)
+
+histo.save("histograma.jpg")
+
