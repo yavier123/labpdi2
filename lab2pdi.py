8aa10c9010335ad36ea1cfef7fac11b8d5287509
yavier123 <yavier123>
2016-04-24 00:32:21 +0000

    3era subida del proyecto con los histogramas

--- lab2pdi.py
+++ lab2pdi.py
@@ -4,7 +4,11 @@ from SimpleCV  import Image, Camera, Display, time
 
 import matplotlib.pyplot as plt
 
+
 cam = Camera()
+
+time.sleep(1)
+
 img = cam.getImage()
 
 img.save("foto1.jpg")
@@ -13,7 +17,7 @@ time.sleep(2)
 
 img2=cam.getImage()
 
-img2.save("foto2.jpg")
+img2.save("foto2.jpg")  
 
 time.sleep(2)
 
@@ -26,6 +30,43 @@ histograma=escaladegris.histogram()
 
 plt.figure(1)
 
-plt.plot(histograma)
+plt.stem(histograma)
 
-plt.show() 
+plt.grid()
+  
+plt.savefig('histogramaengris.png')
+
+time.sleep(1)
+
+#imgsplit= cam.getImage()
+
+(red, green, blue) = img2.splitChannels(False)
+ 
+red_histogram= red.histogram(255)
+
+plt.figure(2)
+
+plt.grid()
+
+plt.stem(red_histogram)
+
+plt.savefig('histogramaenrojo.png')
+
+blue_histogram = blue.histogram(255)
+
+plt.figure(3)
+
+plt.stem(blue_histogram)
+
+plt.savefig('histogramaenazul.png')
+
+green_histogram= green.histogram(255)
+
+plt.figure(4)
+
+plt.stem(green_histogram)
+
+plt.savefig('histogramaenverde.png')
+
+
+
